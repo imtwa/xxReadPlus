d49ca0ff3e0454e6ab49aa879ba45345fa756c2f
imtwa <imtwa@qq.com>
2025-03-13 20:27:48 +0800

    init

---
+++ pages/books/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <up-action-sheet :actions="list" v-model:show="show"></up-action-sheet>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const list = ref([
+  {
+    text: '点赞',
+    color: 'blue',
+    fontSize: 28
+  },
+  {
+    text: '分享'
+  },
+  {
+    text: '评论'
+  }
+]);
+console.log(list.value);
+const show = ref(true);
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import {ref} from 'vue'
+	const title = ref('主页')
+</script>
+
+<style lang="scss">
+	.text-area{
+		margin: 100px;
+		.title{
+			border: aliceblue;
+		}
+	}
+
+</style>
